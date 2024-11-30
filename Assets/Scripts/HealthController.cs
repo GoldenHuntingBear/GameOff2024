730a319db63b3323f1ea28bc5024c50dcf467719
GoldenHuntingBear <beringer.lam@gmail.com>
2024-11-30 11:21:11 +0100

    Death

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -1,14 +1,35 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class HealthController : MonoBehaviour
 {
+    public AudioSource deadSound;
     private float health = 1;
     public Action Dead;
+    private Rigidbody2D _rigidbody;
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
     public void TakeDamage(float damage)
     {
         Debug.Log("Dead");
         health -= damage;
+        _rigidbody.constraints = RigidbodyConstraints2D.FreezeAll;
+        deadSound.Play();
+        StartCoroutine(waitForSound(deadSound));
+    }
+
+    IEnumerator waitForSound(AudioSource source)
+    {
+        //Wait Until Sound has finished playing
+        while (source.isPlaying)
+        {
+            yield return null;
+        }
+
         Dead.Invoke();
+        _rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 }
